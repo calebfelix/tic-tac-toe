120b01956f6a7d9fd4a36d4446037e9bde70d351
caleb <calebfelix38@gmail.com>
2023-08-25 01:52:20 +0530

    driver code updated

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Game = require("./Game")
 
 g1 = new Game()
+g2 = new Game()
 
-//Draw
+// Draw
 console.log(g1.play("0"))
+console.log(g1.play(0))
 console.log(g1.play(1))
 console.log(g1.play(2))
 console.log(g1.play(4))
@@ -13,3 +15,10 @@ console.log(g1.play(6))
 console.log(g1.play(8))
 console.log(g1.play(7))
 console.log(g1.play(7))
+
+// Winner
+console.log(g2.play(0))
+console.log(g2.play(5))
+console.log(g2.play(1))
+console.log(g2.play(4))
+console.log(g2.play(2))
